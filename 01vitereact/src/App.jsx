98eb1aa270420a4198c993e9e932d0b3aeedf1d6
Yogita Sanas <yogitasanas15@gmail.com>
2026-03-14 04:19:48 +0530

    done

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,12 +1,13 @@
 import Chai from "./chai"
 
 function App() {
+  const username = "yogii.."
 
   return (
-  //  <h1>chai aur react with vite</h1>   --only one element can exposed
+  //  <h1>chai aur react with vite</h1>   --only one element can export
   <>
   <Chai/>
-  <h1>chai aur react</h1>
+  <h1>chai aur react {username}</h1>
   <p>test para</p>
   </>
   )
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,9 +1,50 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import React from 'react'
+import {jsx as _jsx} from "react/jsx-runtime.js"   //--createElement() can take like this from jsx
+
+function MyApp(){
+  return(
+    <div>
+      <h1>Custom App !</h1>
+    </div>
+  )
+}
+
+// createRoot(document.getElementById('root')).render(  
+//   <StrictMode>
+//     {/* <App /> */}
+//     <MyApp />
+//    </StrictMode>
+// )
+
+// const reactElement = {             //--not work because render want some syntax
+//     type: 'a',
+//     props: {
+//         href: 'https://google.com',
+//         target: '_blank'
+//     },
+//     children: 'Click me to visit google'
+// }
+
+const anotherElement = (
+  <a href='https://google.com' target='_blank'>visit google</a>
+)
+
+const anotherUser = "React.."
+
+const reactElement = React.createElement(
+  'a',
+  {href: 'https://google.com', target: '_blank'},
+  'click me to visit google',
+  anotherUser
+)
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  // MyApp()
+  // reactElement     //--not work
+  // anotherElement
+  reactElement
+  // <App />
 )
