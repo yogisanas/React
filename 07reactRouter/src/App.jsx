8e60007a752b9f747a61ab168648e70eff0eb8d5
Yogita Sanas <yogitasanas15@gmail.com>
2026-03-25 05:45:44 +0530

    done

---
+++ 07reactRouter/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+import Header from './components/Header/Header'
+import Home from './components/Home/Home'
+import Footer from './components/Footer/Footer'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+  <>
+  {/* <h1 className='bg-green-600 p-4'>React Router</h1> */}
+  {/* <Header />
+  <Home />
+  <Footer /> */}
+  </>
+  )
+}
+
+export default App
