0743fc864036e07c67677d8df202f742a877dcdd
Yogita Sanas <yogitasanas15@gmail.com>
2026-04-06 07:08:34 +0530

    done

---
+++ 11reduxToolkitTodo/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+import AddTodo from './components/AddTodo'
+import Todos from './components/Todos'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <h1>Learn about Redux Toolkit</h1>
+    <AddTodo />
+    <Todos />
+    </>
+  )
+}
+
+export default App
