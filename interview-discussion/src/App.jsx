89b8dfd0ffa492bb192b9efdf6e55e0540b8ac47
Yogita Sanas <yogitasanas15@gmail.com>
2026-06-26 21:29:45 +0530

    interview discussion

---
+++ interview-discussion/src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  const [value, setValue] = useState(1)
+  const [multipliedValue, setMultipliedValue] = useState(1)
+  // let multipliedValue = value * 5  
+
+  const multiplybyfive = () => {
+    setMultipliedValue(value * 5)
+    // setMultipliedValue(multipliedValue * 5)
+    setValue(value + 1)    
+  }
+
+  return (
+   <>
+   <h1>Main Value: {value}</h1>
+   <button 
+   onClick={multiplybyfive}
+   >Click to multiply by 5</button>
+   <h2>Multiplied Value: {multipliedValue}</h2>
+   </>
+  )
+}
+
+export default App
